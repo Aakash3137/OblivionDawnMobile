acb737eb5beae9ddc120330a91c79b4eb123d7b3
unrealprismart03-lang <unreal.prismart03@gmail.com>
2025-12-18 11:14:41 +0530

    Wall Scirpt and Prefab

---
+++ Assets/3. Scripts/4. SinglePlayer/PlaceWalls.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class PlaceWalls : TileUIPanel
+{
+    [SerializeField] private GameObject _wallPrefab;
+    private Tile _currentTile;
+
+    public void PlaceWallAround()
+    {
+        //private Vector2Int _currentTileCords;
+    }
+}
