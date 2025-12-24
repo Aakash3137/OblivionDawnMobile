caf65e2595fa6d1787284672a8e407133dc874ac
gaurav <unreal.prismart03@gmail.com>
2025-12-24 10:12:32 +0530

    wall updated

--- Assets/3. Scripts/4. SinglePlayer/TileUIPanel.cs
+++ Assets/3. Scripts/4. SinglePlayer/TileUIPanel.cs
@@ -155,11 +155,18 @@
 
 
 
+using System.Collections.Generic;
 using UnityEngine;
 
 public class TileUIPanel : MonoBehaviour
 {
     [SerializeField] private BuildPanel buildPanel;
+
+    [SerializeField] private WallParent _wallPrefab;
+
+
+    private bool _mainWallPlaced = false;
+    private float _wallYOffset = 1f;
     private Tile currentTile;
 
     public void Open(Tile tile)
@@ -193,7 +200,76 @@ public class TileUIPanel : MonoBehaviour
 
         // Fade out build panel
         if (buildPanel != null) buildPanel.gameObject.SetActive(false);
+        PlaceWallsOnMainBuilding();
+        PlaceWalls();
 
         Close();
     }
+
+
+    // Wall logic (unchanged)
+    private void PlaceWalls()
+    {
+        Vector3 _currentTileCords = currentTile.transform.position;
+
+        List<Vector2Int> adjacentTileCords = CubeGridManager.Instance.GetCardinalNeighbors(
+            new Vector2Int((int)(_currentTileCords.x / CubeGridManager.Instance.cellSize),
+                           (int)(_currentTileCords.z / CubeGridManager.Instance.cellSize)));
+
+        List<Tile> adjacentTiles = new List<Tile>();
+
+        foreach (var cord in adjacentTileCords)
+        {
+            if (CubeGridManager.Instance.GetCube(cord) == null)
+                adjacentTiles.Add(new GameObject().AddComponent<Tile>());
+            else
+                adjacentTiles.Add(CubeGridManager.Instance.GetCube(cord).GetComponent<Tile>());
+        }
+
+        WallParent currentWall = Instantiate(_wallPrefab,
+            new Vector3(_currentTileCords.x, _wallYOffset, _currentTileCords.z),
+            Quaternion.identity, currentTile.transform);
+
+        for (int i = 0; i < adjacentTiles.Count; i++)
+        {
+            if (adjacentTiles[i].ownerSide == Side.Enemy)
+                continue;
+
+            if (adjacentTiles[i].hasBuilding)
+            {
+                switch (i)
+                {
+                    case 0:
+                        currentWall.DisableWall(0);
+                        adjacentTiles[i].GetComponentInChildren<WallParent>().DisableWall(1);
+                        break;
+                    case 1:
+                        currentWall.DisableWall(1);
+                        adjacentTiles[i].GetComponentInChildren<WallParent>().DisableWall(0);
+                        break;
+                    case 2:
+                        currentWall.DisableWall(2);
+                        adjacentTiles[i].GetComponentInChildren<WallParent>().DisableWall(3);
+                        break;
+                    case 3:
+                        currentWall.DisableWall(3);
+                        adjacentTiles[i].GetComponentInChildren<WallParent>().DisableWall(2);
+                        break;
+                }
+            }
+        }
+    }
+
+    private void PlaceWallsOnMainBuilding()
+    {
+        if (_mainWallPlaced) return;
+
+        Transform mainBuildingTile = MainBuildingSpawner.Instance.playerSpawnPoint;
+        Instantiate(_wallPrefab,
+            new Vector3(mainBuildingTile.position.x, _wallYOffset, mainBuildingTile.position.z),
+            Quaternion.identity, mainBuildingTile);
+
+        _mainWallPlaced = true;
+    }
 }
+
