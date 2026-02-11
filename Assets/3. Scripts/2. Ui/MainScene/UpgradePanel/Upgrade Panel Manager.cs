0691da528c538d247dd1afecfaa226549e59440d
unrealprismart03-lang <unreal.prismart03@gmail.com>
2026-02-11 17:59:47 +0530

    Added catapult visual changes to cards

--- Assets/3. Scripts/2. Ui/MainScene/UpgradePanel/Upgrade Panel Manager.cs
+++ Assets/3. Scripts/2. Ui/MainScene/UpgradePanel/Upgrade Panel Manager.cs
@@ -6,5 +6,17 @@ public class UpgradePanelManager : MonoBehaviour
     [SerializeField] private List<UnitProduceStatsSO> unitScriptables;
     [SerializeField] private List<BuildingDataSO> buildingScriptables;
 
+    [SerializeField] private CardUpgrade buildingCard;
+    [SerializeField] private CardUpgrade unitCard;
+
+
+    public void CreateBuildingCards()
+    {
+        foreach (var item in buildingScriptables)
+        {
+            CardUpgrade card = Instantiate(buildingCard, transform);
+            // card.GetComponent<BuildingCard>().Initialize(item);
+        }
+    }
 
 }
