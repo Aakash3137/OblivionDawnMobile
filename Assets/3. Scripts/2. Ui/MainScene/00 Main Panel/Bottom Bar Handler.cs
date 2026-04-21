0a6f11f72b4fb8ac6c5675ec72fc423c8835d375
unrealprismart06-cmd <unreal.prismart06@gmail.com>
2026-04-21 18:04:36 +0530

    Main Scene Clean up UI scripts folder structure

---
+++ Assets/3. Scripts/2. Ui/MainScene/00 Main Panel/Bottom Bar Handler.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BottomBarHandler : MonoBehaviour
+{
+    [SerializeField] private Toggle[] toggles;
+
+    private void Start()
+    {
+        for (int i = 0; i < toggles.Length; i++)
+        {
+            int index = i;
+            toggles[i].onValueChanged.AddListener(isOn => { if (isOn) TogglePanel(index); });
+        }
+
+        var defaultPanel = (int)CenterScrollHandler.Instance.defaultPanel;
+        toggles[defaultPanel].SetIsOnWithoutNotify(true);
+
+        CenterScrollHandler.Instance.OnPanelChanged += OnPanelChanged;
+    }
+
+    private void OnPanelChanged(int index)
+    {
+        for (int i = 0; i < toggles.Length; i++)
+            toggles[i].SetIsOnWithoutNotify(i == index);
+    }
+
+    public void TogglePanel(int index)
+    {
+        CenterScrollHandler.Instance.SetPanel(index);
+    }
+
+    private void OnDestroy()
+    {
+        if (CenterScrollHandler.Instance != null)
+            CenterScrollHandler.Instance.OnPanelChanged -= OnPanelChanged;
+    }
+}
---
+++ Assets/3. Scripts/2. Ui/MainScene/03 Battle Panel/Battle Panel Manager.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattlePanelManager : MonoBehaviour
+{
+    [SerializeField] private Button playButton;
+}
