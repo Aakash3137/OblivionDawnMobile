8be1cceea2dd8d4e12d40be3130a48eff7489d96
unrealprismart06-cmd <unreal.prismart06@gmail.com>
2026-04-22 11:04:01 +0530

    Implemented Randomise start resources

--- Assets/3. Scripts/4. SinglePlayer/EnemyAI/EnemyResourceManager.cs
+++ Assets/3. Scripts/4. SinglePlayer/EnemyAI/EnemyResourceManager.cs
@@ -1,5 +1,3 @@
-using Sirenix.OdinInspector;
-
 public class EnemyResourceManager : ResourceManager
 {
     public static EnemyResourceManager Instance;
@@ -8,28 +6,4 @@ public class EnemyResourceManager : ResourceManager
     {
         Instance = this;
     }
-
-    #region  ContextMenu
-    [Button]
-    public void ResetResources()
-    {
-        startingResources[0].resourceAmount = 100;
-        startingResources[1].resourceAmount = 100;
-        startingResources[2].resourceAmount = 100;
-        startingResources[3].resourceAmount = 100;
-
-        SetResources(startingResources);
-    }
-
-    [Button]
-    public void HackResources()
-    {
-        startingResources[0].resourceAmount = 999;
-        startingResources[1].resourceAmount = 999;
-        startingResources[2].resourceAmount = 999;
-        startingResources[3].resourceAmount = 999;
-
-        SetResources(startingResources);
-    }
-    #endregion
 }
--- Assets/3. Scripts/4. SinglePlayer/Resource Management/PlayerResourceManager.cs
+++ Assets/3. Scripts/4. SinglePlayer/Resource Management/PlayerResourceManager.cs
@@ -1,5 +1,3 @@
-using Sirenix.OdinInspector;
-
 public class PlayerResourceManager : ResourceManager
 {
     public static PlayerResourceManager Instance;
@@ -8,28 +6,4 @@ public class PlayerResourceManager : ResourceManager
     {
         Instance = this;
     }
-
-    #region  ContextMenu
-    [Button]
-    public void ResetResources()
-    {
-        startingResources[0].resourceAmount = 100;
-        startingResources[1].resourceAmount = 100;
-        startingResources[2].resourceAmount = 100;
-        startingResources[3].resourceAmount = 100;
-
-        SetResources(startingResources);
-    }
-
-    [Button]
-    public void HackResources()
-    {
-        startingResources[0].resourceAmount = 999;
-        startingResources[1].resourceAmount = 999;
-        startingResources[2].resourceAmount = 999;
-        startingResources[3].resourceAmount = 999;
-
-        SetResources(startingResources);
-    }
-    #endregion
 }
